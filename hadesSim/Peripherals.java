3ca455aad89204077df260350e4529fe1f56dd1c
itsJayBird <60047254+itsJayBird@users.noreply.github.com>
2020-01-19 01:40:19 -0600

    Add files via upload

--- hadesSim/Peripherals.java
+++ hadesSim/Peripherals.java
@@ -47,14 +47,16 @@ public class Peripherals {
 		
 			case 'O':
 				
-				int[] omegaStrength = { 0, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15500, 17000, 18500, 20000, 21500 };
+				int[] omegaStrength = { 0, 8000, 9000, 10000, 11000, 12000, 13000, 
+										14000, 15500, 17000, 18500, 20000, 21500 };
 				
 				shieldStrength = omegaStrength[shieldLevel];
 			break;
 			
 			case 'P':
 				
-				int[] passiveStrength = { 0, 5000, 6000, 7000, 8000, 9000, 10000, 11500, 13000, 14500, 16000, 17500, 1900 };
+				int[] passiveStrength = { 0, 5000, 6000, 7000, 8000, 9000, 10000, 
+										  11500, 13000, 14500, 16000, 17500, 1900 };
 				
 				shieldStrength = passiveStrength[shieldLevel];
 			break;
@@ -69,7 +71,8 @@ public class Peripherals {
 		
 			case 'B':
 		
-				int[] battStr = { 0, 100, 120, 140, 160, 180, 210, 250, 285, 315, 340, 365, 390 };
+				int[] battStr = { 0, 100, 120, 140, 160, 180, 
+								  210, 250, 285, 315, 340, 365, 390 };
 			
 				weaponStrength = battStr[weaponLevel];
 			break;
@@ -77,7 +80,7 @@ public class Peripherals {
 			case 'L':
 		
 				int[] lasStr = { 0, 80, 200, 90, 240, 100, 280, 120, 325, 140, 360, 160, 400, 180, 
-						 500, 200, 600, 225, 700, 250, 800, 275, 900, 300, 1000 };
+						         500, 200, 600, 225, 700, 250, 800, 275, 900, 300, 1000 };
 		
 				weaponStrength = lasStr[weaponLevel];
 			break;
