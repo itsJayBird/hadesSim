46530d42613e84a8183f88511948cf6b888a24a4
itsJayBird <60047254+itsJayBird@users.noreply.github.com>
2020-01-21 09:15:51 -0600

    Add files via upload

--- hadesSim/BattleSim.java
+++ hadesSim/BattleSim.java
@@ -32,13 +32,30 @@ public class BattleSim {
 		
 		Scanner in = new Scanner(System.in);
 		in.useDelimiter("\n");
-		System.out.println("  Weapons   |   Shields");
-		System.out.println(" Batt - bat | Omega   - omg \n Las  - las | Passive - pas \n Mass - mas | Delta   - dlt \n            | Mirror  - mir");
-		System.out.println("ex: lv 4 bs with omega 4 and batt 5 vs lv 4 bs with delta 3 and laser 6 \n BS4:OMG4:BAT5,BS4:DLT3:LAS5");
-		String init = in.next();
-		init = init.replaceAll("\\s+","");
-		init = init.toUpperCase();
-		shipStats = init;
+		boolean incorrectInput = false;
+		
+		do {
+			System.out.println("  Weapons   |   Shields");
+			System.out.println(" Batt - bat | Omega   - omg \n Las  - las | Passive - pas \n Mass - mas | Delta   - dlt \n            | Mirror  - mir");
+			System.out.println("ex: lv 4 bs with omega 4 and batt 5 vs lv 4 bs with delta 3 and laser 6 \n BS4:OMG4:BAT5,BS4:DLT3:LAS5");
+			String init = in.next();
+			init = init.replaceAll("\\s+","");
+			init = init.toUpperCase();
+			shipStats = init;
+		
+			try {
+				makeShips();
+				@SuppressWarnings("unused")
+				Battleship bs1 = new Battleship(bsOneLv, bsOneWeaponType, bsOneWeaponLevel, bsOneShieldType, bsOneShieldLevel);
+				@SuppressWarnings("unused")
+				Battleship bs2 = new Battleship(bsTwoLv, bsTwoWeaponType, bsTwoWeaponLevel, bsTwoShieldType, bsTwoShieldLevel);
+			}
+			catch(Exception e) {
+				System.out.println("Incorrect input!");
+				incorrectInput = true;
+			}
+		}while(incorrectInput == true);
+		
 		System.out.println("Want to randomize who attacks first? Y/N");
 		String randomizer = in.next();
 		randomizer = randomizer.toUpperCase();
