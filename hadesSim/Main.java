2766555b039d0da6f299ec3c019e27bb7f2f3f65
itsJayBird <60047254+itsJayBird@users.noreply.github.com>
2020-01-22 01:36:01 -0600

    Add files via upload

--- hadesSim/Main.java
+++ hadesSim/Main.java
@@ -4,24 +4,40 @@ import java.util.Scanner;
 
 public class Main {
 
-	@SuppressWarnings("resource")
+	@SuppressWarnings({ "resource", "null" })
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		boolean again = true;
+		boolean hasError = false;
 		
 		while(again == true){
 			
 			BattleSim.main(args);
 			
-			System.out.println("Simulate again? Y/N");
-			String input = in.next();
-			input = input.toUpperCase();
+			System.out.println("\nSimulate again? Y/N");
 			
-			if(input.contains("Y")) {
-				again = true;
-			} else if(input.contains("N")) {
-				again = false;
-			}
+			do {
+				
+				hasError = false;
+				
+				String input = in.next();
+				input = input.toUpperCase();
+			
+				if(input.contains("N")) {
+					again = false;
+				}
+			
+				try {
+					if(input.contains("N") == false && input.contains("Y") == false) {
+						again = (Boolean) null;
+					}
+				}
+				catch(NullPointerException e) {
+					System.out.println("Incorrect input, Try again!\n");
+					hasError = true;
+				}
+				
+			}while(hasError == true);
 			
 		}
 
