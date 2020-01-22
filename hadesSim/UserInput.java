169b7a716674b8d5639bd938732df16149b37353
itsJayBird <60047254+itsJayBird@users.noreply.github.com>
2020-01-22 13:45:42 -0600

    Update UserInput.java

--- hadesSim/UserInput.java
+++ hadesSim/UserInput.java
@@ -1,15 +1,17 @@
 package hadesSim;
 import java.util.Scanner;
-
 import hadesSim.Battleship;
 
 public class UserInput {
-
+	//vars for setting up final ship stats, ship stats will be input
+	//side1, side2 is going to be the final ship stats transfered over to math class
 	private String shipStats;
-	
 	private Battleship side1;
 	private Battleship side2;
 	
+	// stats to build the first ship
+	// bsHull is going to be final hull value getting passed to the ship 
+	// all other vars are to build Battleship( int bsLv, String weapon, int weaponLv, String shield, int shieldLv )
 	private int bsOneLv;
 	private String bsOneWeaponType;
 	private int bsOneWeaponLevel;
@@ -19,6 +21,7 @@ public class UserInput {
 	private double bsOneShieldMultiplier;
 	private double bsOneHull;
 	
+	// vars to build ship 2
 	private int bsTwoLv;
 	private String bsTwoWeaponType;
 	private int bsTwoWeaponLevel;
@@ -28,27 +31,23 @@ public class UserInput {
 	private double bsTwoShieldMultiplier;
 	private double bsTwoHull;
 	
+	//passes if rng is enabled
 	private boolean rng;
-	
-	
+
 	@SuppressWarnings("resource")
-	public void takeUserInput() {
-		
+	public void takeUserInput() {	
 		Scanner in = new Scanner(System.in);
 		in.useDelimiter("\n");
-
 		String init = in.next();
+		//takes input, removes spaces, and sets all to uppercase for parsing
 		init = init.replaceAll("\\s+","");
 		init = init.toUpperCase();
 		shipStats = init;
-		
 	}
 	
 	@SuppressWarnings({ "null", "resource" })
 	public void setRNG() {
-		
 		Scanner in = new Scanner(System.in);
-		
 		System.out.println("Want to randomize who attacks first? Y/N");
 		String randomizer = in.next();
 		randomizer = randomizer.toUpperCase();
@@ -59,12 +58,10 @@ public class UserInput {
 			rng = false;
 		} else {
 			rng = (Boolean) null;
-		}
-		
+		}	
 	}
 	
 	public void makeShips() {
-		
 		// example input BS6:OMG5:LAS5VBS5:PAS3:BAT5
 		// second input BS6@95:DLT4:BAT10VBS4@50:PAS10:LAS7
 		// Separates the two ships
@@ -74,17 +71,15 @@ public class UserInput {
 		String s2 = a[1];
 		
 		// determining first ships attributes 
-		//start by separating each attribute
+		// start by separating each attribute
 		String delim2 = ":";
 		String[] ship1 = s1.split(delim2);
-		
 		// first chunk of input ex. BS6 OR BS6@50
 		String bs1 = ship1[0];
 		String lv = bs1.substring(2,3);
 		int bsLevel = Integer.parseInt(lv);
 		double bsHealth = 0;
 		String bsHP = "100";
-		
 		// building shield string ex. OMG5 OR OMG10, substring pulls first 3 to assign type
 		String shield1 = ship1[1];
 		String shieldType = shield1.substring(0,3);
@@ -92,7 +87,6 @@ public class UserInput {
 		int weaponLevel = 1;
 		double shieldHealth = 0;
 		String shieldHP = "100";
-		
 		// building weapon string ex. BAT5 OR BAT10, substring pulls first 3 to assign type
 		String weapon1 = ship1[2];
 		String weaponType = weapon1.substring(0,3);
@@ -105,20 +99,18 @@ public class UserInput {
 			bsHP = bs1.substring((bs1.indexOf('@') + 1), bs1.length());
 		}
 		bsHealth = Integer.parseInt(bsHP) / 100.0;
-		
 		// if input ex. OMG4 uses first bit of code to take level
 		// if input ex. OMG10 uses second bit to take the level
 		if(shield1.length() == 4) {
 			String shlv = shield1.substring(3,4);
 			shieldLevel = Integer.parseInt(shlv);
 		}
-		
 		if(shield1.length() == 5) {
 			String shieldLv = shield1.substring(3,5);
 			shieldLevel = Integer.parseInt(shieldLv);
 		}
 		
-		// same as with hp, if no @ assums 100% hp on shield
+		// same as with hp, if no @ assumes 100% hp on shield
 		// if @ pulls the number after to set hp multiplier
 		if(shield1.contains("@") == false) {
 			shieldHealth = 100;
@@ -133,29 +125,23 @@ public class UserInput {
 			String wlv = weapon1.substring(3,4);
 			weaponLevel = Integer.parseInt(wlv);
 		}
-		
 		if(weapon1.length() == 5) {
 			String weaponLv = weapon1.substring(3,5);
 			weaponLevel = Integer.parseInt(weaponLv);
 		}	
-		
-		
 		// determine ship 2 attributes uses same logic as ship 1 values
 		String[] ship2 = s2.split(delim2);
-		
 		String bs2 = ship2[0];
 		String lv2 = bs2.substring(2,3);
 		int bsLevel2 = Integer.parseInt(lv2);
 		double bs2Health = 0;
 		String bs2HP = "100";
-		
 		String shield2 = ship2[1];
 		String shieldType2 = shield2.substring(0,3);
 		int shieldLevel2 = 1;
 		int weaponLevel2 = 1;
 		double shield2Health = 0;
 		String shield2HP = "100";
-		
 		String weapon2 = ship2[2];
 		String weaponType2 = weapon2.substring(0,3);
 		
@@ -193,8 +179,7 @@ public class UserInput {
 			String weaponLv2 = weapon2.substring(3,5);
 			weaponLevel2 = Integer.parseInt(weaponLv2);
 		}
-		
-		
+
 		// setting all the values into memory
 		bsOneLv = bsLevel;
 		bsOneShieldType = shieldType;
@@ -203,8 +188,6 @@ public class UserInput {
 		bsOneWeaponLevel = weaponLevel;
 		bsOneHealthMultiplier = bsHealth;
 		bsOneShieldMultiplier = shieldHealth;
-		
-		
 		bsTwoLv = bsLevel2;
 		bsTwoShieldType = shieldType2;
 		bsTwoShieldLevel = shieldLevel2;
@@ -212,138 +195,81 @@ public class UserInput {
 		bsTwoWeaponLevel = weaponLevel2;
 		bsTwoHealthMultiplier = bs2Health;
 		bsTwoShieldMultiplier = shield2Health;
-		
+		 
+		// creates ships based on values inputted above
 		Battleship firstShip = new Battleship(getSide1Lv(), getSide1WeaponType(), getSide1WeaponLv(), getSide1ShieldType(), getSide1ShieldLv());
 		Battleship secondShip = new Battleship(getSide2Lv(), getSide2WeaponType(), getSide2WeaponLv(), getSide2ShieldType(), getSide2ShieldLv());
-	
+		// setting the weapon values from peripheral class to pass through the battleship class
 		firstShip.setWeapon(firstShip);
 		firstShip.setShield(firstShip);
 		secondShip.setWeapon(secondShip);
 		secondShip.setShield(secondShip);
-		
+		// setting the final hull strength
 		bsOneHull = (firstShip.getHull() * bsHealth) + (firstShip.getShield() * shieldHealth);
 		bsTwoHull = (secondShip.getHull() * bs2Health) + (secondShip.getShield() * shield2Health);
-		
 		firstShip.setFinalHull(bsOneHull);
 		secondShip.setFinalHull(bsTwoHull);
-		
+		// passes full ship info to private var for return
 		side1 = firstShip;
-		side2 = secondShip;
-		
+		side2 = secondShip;	
 	}
-
-	public Battleship getSide1() {
-		
+	
+	public Battleship getSide1() {	
 		return side1;
-		
 	}
-	
 	public Battleship getSide2() {
-		
 		return side2;
-		
 	}
-	
 	public int getSide1Lv() {
-		
-		return bsOneLv;
-		
+		return bsOneLv;	
 	}
-	
 	public int getSide2Lv() {
-		
-		return bsTwoLv;
-		
+		return bsTwoLv;	
 	}
-	
 	public int getSide1WeaponLv() {
-		
 		return bsOneWeaponLevel;
-		
 	}
-	
 	public int getSide2WeaponLv() {
-		
 		return bsTwoWeaponLevel;
-		
 	}
-	
 	public int getSide1ShieldLv() {
-		
 		return bsOneShieldLevel;
-		
 	}
-	
 	public int getSide2ShieldLv() {
-	
 		return bsTwoShieldLevel;
-	
 	}
-
-	public String getSide1WeaponType() {
-		
-		return bsOneWeaponType;
-		
+	public String getSide1WeaponType() {	
+		return bsOneWeaponType;	
 	}
-	
 	public String getSide2WeaponType() {
-		
 		return bsTwoWeaponType;
-		
 	}
-	
-	public String getSide1ShieldType() {
-		
-		return bsOneShieldType;
-		
+	public String getSide1ShieldType() {	
+		return bsOneShieldType;	
 	}
-	
-	public String getSide2ShieldType() {
-		
-		return bsTwoShieldType;
-		
+	public String getSide2ShieldType() {	
+		return bsTwoShieldType;	
 	}
-
-	public double getSide1HealthMultiplier() {
-		
-		return bsOneHealthMultiplier;
-		
+	public double getSide1HealthMultiplier() {	
+		return bsOneHealthMultiplier;	
 	}
-	
-	public double getSide1ShieldMultiplier() {
-		
-		return bsOneShieldMultiplier;
-		
+	public double getSide1ShieldMultiplier() {	
+		return bsOneShieldMultiplier;	
 	}
-
-	public double getSide2HealthMultiplier() {
-		
-		return bsTwoHealthMultiplier;
-		
+	public double getSide2HealthMultiplier() {	
+		return bsTwoHealthMultiplier;	
 	}
-
-	public double getSide2ShieldMultiplier() {
-		
-		return bsTwoShieldMultiplier;
-		
+	public double getSide2ShieldMultiplier() {	
+		return bsTwoShieldMultiplier;	
 	}
-
-	public boolean getRNG() {
-		
-		return rng;
-		
+	public boolean getRNG() {	
+		return rng;	
 	}
-	
-	public double getHullStr1() {
-		
-		return bsOneHull;
-		
+	public double getHullStr1() {	
+		return bsOneHull;	
 	}
-	
-	public double getHullStr2() {
-		
-		return bsTwoHull;
-		
+	public double getHullStr2() {	
+		return bsTwoHull;	
 	}
 	
 }
