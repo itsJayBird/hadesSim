54e350425d60141f83723d8acf611c77211f26fb
itsJayBird <60047254+itsJayBird@users.noreply.github.com>
2020-01-22 01:27:42 -0600

    Add files via upload

--- hadesSim/Main.java
+++ hadesSim/Main.java
@@ -1,10 +1,29 @@
 package hadesSim;
 
+import java.util.Scanner;
+
 public class Main {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		boolean again = true;
 		
+		while(again == true){
+			
 			BattleSim.main(args);
+			
+			System.out.println("Simulate again? Y/N");
+			String input = in.next();
+			input = input.toUpperCase();
+			
+			if(input.contains("Y")) {
+				again = true;
+			} else if(input.contains("N")) {
+				again = false;
+			}
+			
+		}
 
 	}
 }
