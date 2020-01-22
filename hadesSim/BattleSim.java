f4d88ce4b64e5c20cbe4db79f600bbd5dab95de2
itsJayBird <60047254+itsJayBird@users.noreply.github.com>
2020-01-22 13:55:23 -0600

    Update BattleSim.java

--- hadesSim/BattleSim.java
+++ hadesSim/BattleSim.java
@@ -2,14 +2,11 @@ package hadesSim;
 
 public class BattleSim {
 	
-	public BattleSim() {
-		
-	}
+	public BattleSim() {}
 	
 	public static void main(String[] args) {
 		UserInput a = new UserInput();
 		boolean hasError = false;
-		
 		System.out.println("\nInput in form of BSLVL:SHIELDLVL:WEAPONLVL V BSLVL:SHIELDLVL:WEAPONLVL \n");
 		System.out.println("Modify shield or battleship hull using @XX \n");
 		System.out.println("Example:  BS4@75:OMG10:BAT8 V BS5:DLT11%75:LAS9");
@@ -25,7 +22,7 @@ public class BattleSim {
 				System.out.println("Input error, Try again!");
 				hasError = true;
 			}			
-		}while(hasError == true);
+		}while(hasError == true); // this block of code check to see if you input the information incorrectly
 	
 		a.makeShips();
 		
@@ -39,18 +36,12 @@ public class BattleSim {
 				hasError = true;
 			}
 			
-		}while(hasError == true);
+		}while(hasError == true); // this checks to see if you didn't input Y or N
 		
 		BattleMath sim = new BattleMath(a.getSide1(), a.getSide2(), a.getRNG());
-
 		sim.doMath();
-		
 		System.out.println(sim.determineWinner() + " " + sim.determineWinPct() + "% of the time!");
-		
-		if(a.getRNG() == true) System.out.println("Side 1 attacks first " + sim.findNumHeads() + "/" + "1000 times!");
-		
-		
+		//checks if rng is enabled, if it is, will tell you how many times side 1 attacks first
+		if(a.getRNG() == true) System.out.println("Side 1 attacks first " + sim.findNumHeads() + "/" + "1000 times!");	
 	}
-
-	
 }
